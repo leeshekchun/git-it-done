12443220db42f4ab7814feae78bc1ff7a50a1c4a
ShekChun Lee <leeshekchun@gmail.com>
2022-04-18 05:27:40 -0600

    6.2.5

---
+++ assets/js/homepage.js
@@ -0,0 +1,44 @@
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      displayRepos(data, user);
+    });
+  });
+};
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+  // clear old content
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent = searchTerm;
+};
+
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
